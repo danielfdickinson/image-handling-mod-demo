a899b0ab2aab5e33d32915531c91e5731149cc7f
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-07-17 17:23:17 -0400

    Tweak Netlify build script and drop check-links
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/netlify_build.sh
+++ scripts/netlify_build.sh
@@ -8,9 +8,10 @@ set -o pipefail
 git lfs install
 
 bash ./tests/scripts/hugo-audit.sh
-bash ./tests/scripts/check-internal-links.sh
 
 rm -rf public exampleSite/public
+
+export HUGO_PARAMS_DEFAULTCANONICAL="$URL"
 
 if [ "$CONTEXT" = "production" ]; then
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$URL"
@@ -18,4 +19,4 @@ else
 	export HUGO_PARAMS_DEPLOYEDBASEURL="$DEPLOY_PRIME_URL"
 fi
 
-HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $HUGO_PARAMS_DEPLOYEDBASEURL
+HUGO_RESOURCEDIR="$(pwd)/resources" hugo --gc --minify -b $HUGO_PARAMS_DEPLOYEDBASEURL --destination "$(pwd)"/public
